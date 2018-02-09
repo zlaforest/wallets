99c3f2af58b22bf4fb96eb27bd4186c4d49ac429
zlaforest <zoe.laforest@gmail.com>
2018-02-09 11:32:09 +0100

    cors filter

---
+++ cryptos-java/src/main/java/io/pax/cryptos/ws/CorsFilter.java
@@ -0,0 +1,42 @@
+package io.pax.cryptos.ws;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by nicorama and others on 09/02/2018.
+ */
+@WebFilter(asyncSupported = true, urlPatterns = { "/*" })
+public class CorsFilter implements Filter {
+
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        // pass the request along the filter chain
+        chain.doFilter(request, servletResponse);
+    }
+
+
+    public void init(FilterConfig fConfig) throws ServletException {}
+
+    public void destroy() {}
+
+}
